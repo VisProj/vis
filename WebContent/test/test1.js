63595e47e8c4106fe4b69d59760d09d53b516301
Ebraheem Kahskoush <ebraheem@kashkoush.top>
2016-08-24 00:57:13 +0300

    testing how to connect between two js file

---
+++ WebContent/test/test1.js
@@ -0,0 +1,10 @@
+var x=5;
+function bla(y){
+	console.log("from function 1 res =" + y);
+
+}
+
+function init(){
+ 	al(x);
+}
+window.onLoad =init();
---
+++ WebContent/test/test2.js
@@ -0,0 +1,7 @@
+function al(x){
+	console.log("from function 2 res =" + x);
+}
+function init(){
+	bla("s555");
+}
+window.onLoad =init();
